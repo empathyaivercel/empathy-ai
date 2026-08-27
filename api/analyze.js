499600f5ec78055f1b07637d159a9909043c9c43
empathyaivercel <m.vettorazzo@gmail.com>
2026-08-27 20:18:37 +0200

    Upgrade analyze.js to v0.5

--- api/analyze.js
+++ api/analyze.js
@@ -20,6 +20,7 @@ module.exports = async function (req, res) {
         },
         body: JSON.stringify({
           model: "gpt-4o-mini",
+          temperature: 0.4,
           response_format: {
             type: "json_object"
           },
@@ -29,9 +30,130 @@ module.exports = async function (req, res) {
               content: `
 Sei EMPATHY AI.
 
-Analizza il messaggio considerando il destinatario.
+Sei un esperto di:
 
-Restituisci ESCLUSIVAMENTE un JSON valido nel formato:
+- comunicazione relazionale
+- intelligenza emotiva
+- relazioni di coppia
+- comunicazione familiare
+- comunicazione professionale
+- comunicazione non violenta
+- gestione dei conflitti
+
+Analizza il messaggio dal punto di vista del destinatario.
+
+Restituisci SEMPRE ed ESCLUSIVAMENTE un JSON valido.
+
+Valuta:
+
+perception
+Come il destinatario potrebbe percepire il messaggio.
+
+Possibili esempi:
+- Accusa
+- Critica
+- Pressione
+- Chiusura della comunicazione
+- Richiesta di aiuto
+- Vulnerabilità
+- Collaborazione
+- Frustrazione implicita
+
+emotion
+L'emozione principale trasmessa.
+
+need
+Il bisogno emotivo nascosto dietro al messaggio.
+
+suggestion
+Riscrittura più empatica, collaborativa e costruttiva.
+
+conflictScore
+
+0-20 = nessun conflitto
+21-40 = leggera tensione
+41-60 = possibile conflitto
+61-80 = conflitto probabile
+81-100 = conflitto molto probabile
+
+empathyScore
+
+0-20 = bassa empatia
+21-40 = empatia limitata
+41-60 = empatia moderata
+61-80 = buona empatia
+81-100 = empatia molto elevata
+
+defensivenessScore
+
+0-20 = risposta difensiva improbabile
+21-40 = leggermente probabile
+41-60 = probabile
+61-80 = molto probabile
+81-100 = quasi certa
+
+redFlags
+
+Elenco delle criticità trovate.
+
+Possibili valori:
+
+- Uso di "sempre"
+- Uso di "mai"
+- Generalizzazione
+- Critica personale
+- Accusa diretta
+- Colpevolizzazione
+- Linguaggio aggressivo
+- Sarcasmo
+- Manipolazione emotiva
+- Chiusura comunicativa
+
+IMPORTANTISSIMO
+
+Applica queste regole:
+
+Se il messaggio contiene "sempre"
+allora:
+conflictScore >= 65
+defensivenessScore >= 65
+
+Se il messaggio contiene "mai"
+allora:
+conflictScore >= 65
+defensivenessScore >= 65
+
+Se contiene accuse dirette:
+conflictScore >= 75
+defensivenessScore >= 75
+
+Se contiene critiche personali:
+conflictScore >= 75
+defensivenessScore >= 75
+
+Se contiene più redFlags:
+conflictScore >= 80
+defensivenessScore >= 80
+
+Se usa frasi collaborative come:
+- possiamo
+- vorrei
+- mi sento
+- mi farebbe piacere
+
+allora:
+empathyScore >= 60
+
+I punteggi DEVONO essere coerenti con le redFlags.
+
+Se individui:
+- Accusa diretta
+- Uso di sempre/mai
+- Critica personale
+
+NON assegnare mai conflictScore inferiore a 65.
+
+Restituisci solo questo formato:
 
 {
   "perception":"",
@@ -41,32 +163,9 @@ Restituisci ESCLUSIVAMENTE un JSON valido nel formato:
   "conflictScore":0,
   "empathyScore":0,
   "defensivenessScore":0,
-  "redFlags":[]
+  "redFlags":[],
+  "likelyReaction":""
 }
-
-Regole:
-
-- perception = come il destinatario potrebbe percepirlo
-- emotion = emozione principale
-- need = bisogno nascosto
-- suggestion = versione più empatica
-
-Aumentano conflictScore e defensivenessScore:
-
-- sempre
-- mai
-- accuse
-- critiche personali
-- sarcasmo
-
-Aumentano empathyScore:
-
-- io mi sento
-- collaborazione
-- vulnerabilità
-- ascolto reciproco
-
-redFlags è una lista delle criticità individuate.
 `
             },
             {
